1b0fc85bcdb4ea4734a931db5e5832faf79bc976
Ohtmane Benyoub <45142479+OhtmaneB@users.noreply.github.com>
2020-09-22 15:46:31 +0100

    Add files via upload

---
+++ FisherIris(K-means).m
@@ -0,0 +1,19 @@
+k=5;
+xxnew=[5.3333 1.333 2.333 3.2223];
+G=load("fisheriris.mat");
+X=G.meas;
+Y=G.species;
+
+for i=1: length(X)
+    %Euclidean distance
+    d(i)=sqrt(sum((xxnew-X(i,:)).^2));
+end
+
+%order d
+[dorder indx] = sort(d); %[X Y]= similar to X,Y= 
+% indx contains index for each element in d order
+
+knn=dorder(1:k);
+knn_indx=indx(1:k);
+X(knn_indx,:);
+Y(knn_indx);
